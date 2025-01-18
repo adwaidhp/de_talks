60ad2a69eb1709031cd202b6c8ce9349d3cfebd9
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2025-01-18 16:49:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return SafeArea(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Navigation(),
       ),
-      home: Navigation(),
     );
   }
 }
