b6a23438d75a389d26ffaa01ea7ac66da22ab261
Adil <adilhaneefa2005@gmail.com>
2025-01-18 13:53:43 +0530

    hehehe

--- lib/pages/supportpage.dart
+++ lib/pages/supportpage.dart
@@ -178,8 +178,8 @@ class SupportPage extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             SvgPicture.asset(
-                              'assets/icons/Support.svg',
-                              height: 40,
+                              'assets/icons/Phone.svg',
+                              height: 30,
                               width: 40,
                             ),
                             const SizedBox(width: 10),
